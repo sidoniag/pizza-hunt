3265b6fc9bca8574afc6305db800ccdac8966588
idonia <sidoniag@gmail.com>
2020-09-16 15:27:43 -0500

    update getter

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const { Schema, model } = require('mongoose');
 
 const PizzaSchema = new Schema({
@@ -9,7 +10,8 @@ const PizzaSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM D, YYYY [at] hh:mm a')
     },
     size: {
         type: String,
@@ -26,6 +28,7 @@ const PizzaSchema = new Schema({
 {
     toJSON: {
         virtuals: true,
+        getters: true
     },
     id: false
 }
