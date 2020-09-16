89f37c424fcfae0b50d0e6ca4679362ebcfee545
idonia <sidoniag@gmail.com>
2020-09-16 15:22:10 -0500

    comment-model complete

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const pizzaController = require('../../controllers/pizza-controller');
+
+const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
+
+router.use('/comments', commentRoutes);
+router.use('/pizzas', pizzaRoutes);
 
 // add prefix of `/pizzas` to routes created in `pizza-routes.js1
 router.use('/pizzas', pizzaRoutes);
