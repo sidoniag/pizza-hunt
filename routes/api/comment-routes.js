48c7ee943731c2c08338b164f52af3afcc3867c1
idonia <sidoniag@gmail.com>
2020-09-17 21:22:24 -0500

    still need to get reply function working

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -10,15 +10,15 @@ const {
 router
     .route('/:pizzaId').post(addComment);
 
+// /api/comments/<pizzaId>/<commentId>/<replyId>
+router
+.route('/:pizzaId/:commentId/:replyId')
+.delete(removeReply);
+
 // /api/comments/<pizzaId>/<commentId>
 router
     .route('/:pizzaId/:commentId')
     .put(addReply)
-    .delete(removeComment);
-
-// /api/comments/<pizzaId>/<commentId>/<replyId>
-router
-    .route('/:pizzaId/:commentId/:replyId')
-    .delete(removeReply);
+    .delete(removeComment)
     
 module.exports = router;
